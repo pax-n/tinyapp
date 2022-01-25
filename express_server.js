27d46d3fb09d40371463b2a4aec1ce42a8db08d2
pax-n <paxton_nguyen@hotmail.com>
2022-01-24 20:36:31 -0500

    shortURL redirect to longURL .get

--- express_server.js
+++ express_server.js
@@ -48,9 +48,8 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  //const longURL = ...
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
-
  
 
