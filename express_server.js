bd7b97240616e93f5459d175e7031b7db15cb3a5
pax-n <paxton_nguyen@hotmail.com>
2022-01-24 20:21:50 -0500

    added post req body

--- express_server.js
+++ express_server.js
@@ -25,6 +25,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -33,3 +38,4 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
