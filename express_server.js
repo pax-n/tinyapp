67801117ff3c7883d1c9467a5cbdf85da2f3a684
pax-n <paxton_nguyen@hotmail.com>
2022-01-25 18:03:03 -0500

    added edit and update .post function

--- express_server.js
+++ express_server.js
@@ -55,4 +55,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
- 
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  const shortURL = req.params.shortURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/urls/:shortURL/update", (req, res) => {
+  const newURL = req.body.longURL;
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = newURL;
+  res.redirect(`/urls`);
+});
+
